b340a67804582a847ead0cbb66a5f705a730492d
Adam <krciladam@seznam.cz>
2026-02-16 15:48:42 +0100

    Added interactible system and Carry system

--- Assets/Scripts/Player/Movement/PlayerController.cs
+++ Assets/Scripts/Player/Movement/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Player.Movement
@@ -7,13 +8,14 @@ namespace Player.Movement
         [SerializeField] private CharacterController controller;
         [SerializeField] private Transform playerCamera;
 
-        [Header("Movement Settings")]
-        [SerializeField] private float speed = 6.0f;
+        [Header("Movement Settings")] [SerializeField]
+        private float speed = 6.0f;
+
         [SerializeField] private float gravity = -9.81f;
         [SerializeField] private float jumpHeight = 1.0f;
 
-        [Header("Mouse Settings")]
-        [SerializeField] private float mouseSensitivity = 100f;
+        [Header("Mouse Settings")] [SerializeField]
+        private float mouseSensitivity = 100f;
 
         private Vector3 _velocity;
         private float _xRotation;
@@ -21,7 +23,7 @@ namespace Player.Movement
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
-            
+
             if (playerCamera == null && Camera.main != null)
             {
                 playerCamera = Camera.main.transform;
@@ -32,6 +34,10 @@ namespace Player.Movement
         {
             HandleLook();
             HandleMovement();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                TryToInteract();
+            }
         }
 
         private void HandleLook()
@@ -41,12 +47,12 @@ namespace Player.Movement
 
             _xRotation -= mouseY;
             _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
-            
+
             if (playerCamera != null)
             {
                 playerCamera.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
             }
-            
+
             transform.Rotate(Vector3.up * mouseX);
         }
 
@@ -59,13 +65,25 @@ namespace Player.Movement
 
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
-            
+
             Vector3 move = transform.right * x + transform.forward * z;
 
             controller.Move(speed * Time.deltaTime * move);
-            
+
             _velocity.y += gravity * Time.deltaTime;
             controller.Move(_velocity * Time.deltaTime);
+        }
+
+
+        private void TryToInteract()
+        {
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 3f))
+            {
+                if (hit.collider.TryGetComponent(out IInteractible interactible))
+                {
+                    interactible.Interact();
+                }
+            }
         }
     }
 }
