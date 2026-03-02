1f60b9371a4607b2d2e8ba3c64a71d2ec438da76
Adam <krciladam@seznam.cz>
2026-03-02 14:39:36 +0100

    Added functions to UI and made a  SceneLoader

---
+++ Assets/Scripts/Scene/LoadingScreenUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreenUI : MonoBehaviour
+{
+    [SerializeField] private LoadingProgressRadio loadingProgressRadio;
+    [SerializeField] private Slider progressSlider;
+    
+    private void OnEnable()
+    {
+        loadingProgressRadio.OnEventRaised += UpdateProgress;
+    }
+    
+    private void OnDisable()
+    {
+        loadingProgressRadio.OnEventRaised -= UpdateProgress;
+    }
+
+    private void UpdateProgress(float obj)
+    {
+        progressSlider.gameObject.SetActive(true);
+        progressSlider.value = obj;
+    }
+}
---
+++ Assets/Scripts/Scene/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private SceneDataSO gameplaySceneData;
+    [SerializeField] private SceneTransitionRadio sceneTransitionRadio;
+    
+    public void OnPlayButtonClicked()
+    {
+        sceneTransitionRadio.RaiseEvent(gameplaySceneData);
+    }
+    
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
