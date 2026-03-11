e227f6f5831454b4276c781fda9963fee1304af9
Adam <krciladam@seznam.cz>
2026-03-11 12:58:32 +0100

    Added mouse setting and volume settings

---
+++ Assets/Scripts/Player/Movement/PlayerConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DroneAssembly.Player.Movement
+{
+    [CreateAssetMenu(menuName = "Player/PlayerConfig")]
+    public class PlayerConfig : ScriptableObject
+    {
+        public float Speed;
+        public float MouseSensitivity;
+        public float VerticalLookLimit;
+    }
+}
