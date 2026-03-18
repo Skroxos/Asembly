5484362a6ae66a8afc78a9da674188d5bfc244c7
Adam <krciladam@seznam.cz>
2026-03-18 13:58:35 +0100

    Updated the SecretTemplate

--- Assets/RegularGame/Scripts/DataBase/Secrets/SecretTemplate.cs
+++ Assets/RegularGame/Scripts/DataBase/Secrets/SecretTemplate.cs
@@ -2,9 +2,9 @@
 {
 public static class SecretTemplate
 {
-    public const string SaveScoreURL = "https://your-api-endpoint.com/save_score.php";
-    public const string SecretKey = "YourSecretKeyHere";
-    public const string GetTopScoresURL = "https://your-api-endpoint.com/get_top10.php";
+    public const string SaveScoreURL = "#YOURSAVESCOREURL#";
+    public const string SecretKey = "#YOURSECRETKEY#";
+    public const string GetTopScoresURL = "#YOURGETTOPSCORESURL#";
 }
     
 }
