11db564b5ab8fcec7fb2d87662d15ace6d6fbb1b
Adam <krciladam@seznam.cz>
2026-03-18 13:49:08 +0100

    Added SecretTemplate for CI

---
+++ Assets/RegularGame/Scripts/DataBase/Secrets/SecretTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace DroneAssembly.DataBase.Secrets
+{
+public static class SecretTemplate
+{
+    public const string SaveScoreURL = "https://your-api-endpoint.com/save_score.php";
+    public const string SecretKey = "YourSecretKeyHere";
+    public const string GetTopScoresURL = "https://your-api-endpoint.com/get_top10.php";
+}
+    
+}
